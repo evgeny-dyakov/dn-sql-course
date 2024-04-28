2ca8eaaab650211c957ae14e1ba1b3b75b90f2fb
e.dyakov <e3001dk@gmail.com>
2024-04-28 21:03:35 +0300

    меняет сервер на browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,15 @@ const fileinclude = require('gulp-file-include')
 const clean = require('gulp-clean')
 const fs = require('fs')
 const sourceMaps = require('gulp-sourcemaps')
-const server = require('gulp-server-livereload')
+const browserSync = require('browser-sync').create()
+
+gulp.task('server', () => {
+  browserSync.init({
+    server: {
+      baseDir: './app/',
+    },
+  })
+})
 
 gulp.task('html', () =>
   gulp
@@ -16,55 +24,49 @@ gulp.task('html', () =>
       })
     )
     .pipe(gulp.dest('./app'))
+    .pipe(browserSync.stream())
 )
 
 gulp.task('sass', () =>
   gulp
-    .src('./app/scss/*.scss')
+    .src('./app/scss/style.scss')
     .pipe(sourceMaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass())
     .pipe(sourceMaps.write())
-    .pipe(gulp.dest('./app'))
+    .pipe(gulp.dest('./app/'))
+    .pipe(browserSync.stream())
 )
 
 gulp.task('watch', () => {
-  gulp.watch('./app/html/**/*.html', gulp.parallel('html'))
   gulp.watch('./app/scss/**/*.scss', gulp.parallel('sass'))
+  gulp.watch('./app/html/**/*.html', gulp.parallel('html'))
+  gulp.watch('./app/**/*.js').on('change', browserSync.reload)
 })
 
-gulp.task('server', () =>
-  gulp
-    .src('./app/')
-    .pipe(server({
-      livereload: true,
-      open: true,
-    })
-  )
-)
-
-gulp.task('htmlClean', done => {
+const htmlClean = () => {
   if (fs.existsSync('./app/index.html')) {
     return gulp
       .src('./app/index.html', {read: false})
       .pipe(clean({forse: true}))
   }
-  done()
-})
+}
 
-gulp.task('cssClean', done => {
+const cssClean = () => {
   if (fs.existsSync('./app/style.css')) {
     return gulp
       .src('./app/style.css', {read: false})
       .pipe(clean({forse: true}))
   }
+}
+
+gulp.task('clean', done => {
+  htmlClean()
+  cssClean()
+
   done()
 })
 
-gulp.task('default', gulp.series(
-  'htmlClean',
-  'cssClean',
-  'sass',
-  'html',
-  'server',
-  'watch'
-))
+gulp.task('default',
+  gulp.series('clean', 'html', 'sass',
+  gulp.parallel('watch', 'server'))
+)
